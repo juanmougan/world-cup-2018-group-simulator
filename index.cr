5680dc2577a01ad1946c4c3c4c4ab60f82f50abd
Juan Manuel Mougan <juanmougan@gmail.com>
2017-11-18 19:20:38 -0300

    Heroku code

--- index.cr
+++ index.cr
@@ -1,5 +1,15 @@
 require "kemal"
 require "./simulator"
+require "option_parser"
+
+bind = "0.0.0.0"
+port = 8080
+
+OptionParser.parse! do |opts|
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    port = opt.to_i
+  end
+end
 
 get "/groups" do |env|
   env.response.content_type = "application/json"
